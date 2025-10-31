d5ca9b6afe124f03fc918adc06b0b4fd4916c26a
Thalia Wood <42354895+polygeist111@users.noreply.github.com>
2025-10-31 16:27:39 -0700

    Image API Initial Commit
    - Untested POST and GET for images
    - No bound on upload storage volume

--- src/services/apis/poisoner_api.py
+++ src/services/apis/poisoner_api.py
@@ -1,19 +1,22 @@
 import markovify
 from http import HTTPStatus
-from flask_restx import Namespace, Resource, fields, reqparse
+from flask import request, send_file
+from flask_restx import Namespace, Resource
+from werkzeug.datastructures import FileStorage
 import core.markov
+import core.images
 
 poison_ns = Namespace("poison", description="Poisoning operations")
 
 
 
 text_in_parser = poison_ns.parser()
-text_in_parser.add_argument("content", type=str)
+text_in_parser.add_argument("content", 
+                            type=str)
 
 text_out_parser = poison_ns.parser()
-text_out_parser.add_argument("numsentences", type=int)
-
-
+text_out_parser.add_argument("numsentences", 
+                             type=int)
 
 @poison_ns.route("/text")
 class PoisonTextApi(Resource):
@@ -23,7 +26,7 @@ class PoisonTextApi(Resource):
 
     @poison_ns.response(HTTPStatus.OK.value, "Object added")
     @poison_ns.expect(text_in_parser)
-    def post(self) -> str:
+    def post(self):
         """
         Add input string to the corpus buffer and active markov chain
         Pass input as a string named "content"
@@ -31,9 +34,10 @@ class PoisonTextApi(Resource):
         args = text_in_parser.parse_args()
         # Add to the model.
         core.markov.addToCorpus(args["content"])
-        return 201
+        return "Resource added", 201
     
-    def get(self) -> str:
+    @poison_ns.expect(text_out_parser)
+    def get(self):
         """
         Return a generated string of X (1 <= X <= 100, default 3) sentences from the markov chain
         Pass number of sentences as an int named "numsentences"
@@ -52,3 +56,53 @@ class PoisonTextApi(Resource):
             output = core.markov.getXSentences(3)
         return output
 
+
+
+image_in_parser = poison_ns.parser()
+image_in_parser.add_argument("image", 
+                             type = FileStorage, 
+                             location = "files", 
+                             required = True, 
+                             help = "Image file to upload to poisoner API")
+
+@poison_ns.route("/images")
+class PoisonImagesApi(Resource):
+    """
+    API for processing poison text data
+    """
+
+    @poison_ns.response(HTTPStatus.OK.value, "Object added")
+    @poison_ns.expect(image_in_parser)
+    def post(self):
+        """
+        Adds input image (not link, actual image) to the poison buffer
+        NOTE: does not poison images. Assumes they have been tampered with ahead of time.
+        """
+        if "image" not in request.files:
+            poison_ns.abort(400, "No image file provided")
+
+        image_file = request.files['image']
+
+        if not image_file.filename:
+            poison_ns.abort(400, "No selected file")
+
+        status = core.images.saveImageFromPost(image_file)
+        if len(status) is 1 and status[0] is True:
+            return "Resource added", 201
+        elif len(status) is 2 and status[0] is False:
+            poison_ns.abort(500, f"Error processing image: {str(status[1])}")
+        else:
+            poison_ns.abort(500, f"Error processing image (bad function return): {str(status[1])}") # this should never happen
+        
+    
+    # @poison_ns.expect(image_out_parser)
+    def get(self):
+        """
+        Return an image and remove it from the buffer
+        """
+        image_path = core.images.getImageFromBuffer()
+        
+        if image_path is "Image not found":
+            poison_ns.abort(404, "Image not found")
+            
+        return send_file(image_path, as_attachment = True)
---
+++ src/services/core/images.py
@@ -0,0 +1,53 @@
+import os
+import time
+import threading
+import random
+from werkzeug.datastructures import FileStorage
+
+image_directory = "data/images/"
+
+def saveImageFromPost(imageIn: FileStorage):
+    write_time = time.time()
+    new_file_path = f"{image_directory}/corpus_{write_time}"
+    try:
+        imageIn.save(new_file_path)
+        return [True]
+    except Exception as e:
+        return [False, e]
+    
+
+
+def getImageFromBuffer() -> str:
+    # Get a list of all files and subdirectories in the given path
+    all_images = os.listdir(image_directory)
+
+    # Filter out directories, keeping only files
+    images = [entry for entry in all_images if os.path.isfile(os.path.join(image_directory, entry))]
+
+    if not images:
+        print(f"No files found in directory '{image_directory}'.")
+        return "No image found"
+
+    # Choose a random file from the list
+    random_filename = random.choice(images)
+
+    # Construct the full path to the random file
+    random_file_path = os.path.join(image_directory, random_filename)
+
+    # Schedule returned image for deletion in 30 seconds (assumes this is sufficient time for a download)
+    timer = threading.Timer(30, deleteUsedResource(random_file_path))
+    timer.start()
+
+    return random_file_path
+
+
+
+def deleteUsedResource(filepath) -> bool:
+    if os.path.exists(filepath):
+        os.remove(filepath)
+        return True
+    print(f"Filepath {filepath} cannot be deleted, does not exist")
+    return False
+
+# TODO: consider breaking random file selection out into core helper function, same with file deletion
+# TODO: add constraints on file addition, like corpora buffer
